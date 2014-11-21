f30672ca859586ad5ba75b302cde5849d04aeef3
hsadhvani <hsadhvani@gmail.com>
2014-11-21 18:57:38 -0500

    imperial

--- ParallelTSP/src/GoogleMapsAPI.java
+++ ParallelTSP/src/GoogleMapsAPI.java
@@ -16,7 +16,7 @@ public class GoogleMapsAPI {
 			DataInputStream inStream;
 
 			url = new URL(
-					"http://maps.googleapis.com/maps/api/distancematrix/json?origins="
+					"http://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="
 							+ locationA.replaceAll(" ", "%20")
 							+ "&destinations="
 							+ locationB.replaceAll(" ", "%20"));

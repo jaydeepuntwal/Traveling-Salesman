d439c066374c7d19f27ee8bc1c8039f97af79d53
hsadhvani <hsadhvani@gmail.com>
2014-11-20 21:39:07 -0500

    updated input

--- ParallelTSP/src/InputGenerator.java
+++ ParallelTSP/src/InputGenerator.java
@@ -1,8 +1,59 @@
+import java.util.Scanner;
 
-public class InputGenerator {
+// Test 2
 
-	public static void main(String[] args) {
+import edu.rit.pj2.IntVbl;
+import edu.rit.pj2.Loop;
+import edu.rit.pj2.Task;
+
+public class InputGenerator extends Task {
+	IntVbl distMat[][];
+
+	public void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		final GoogleMapsAPI gapi = new GoogleMapsAPI();
+
+		final int n = Integer.parseInt(sc.nextLine());
+		distMat = new IntVbl[n][n];
+		final IntVbl distMat[][] = new IntVbl[n][n];
+
+		final String cities[] = new String[n];
+
+		for (int i = 0; i < n; i++) {
+
+			cities[i] = sc.nextLine();
+		}
 		
-	}
+		sc.close();
+		System.out.println(n);
+		for (int i = 0; i < n; i++) {
 
+			System.out.println(cities[i]);
+		}
+
+		parallelFor(0, n - 1).exec(new Loop() {
+
+			public void run(int i) {
+				for (int j = 0; j < n; j++) {
+					int dist = gapi.getDistance(cities[i], cities[j]);
+					distMat[i][j] = new IntVbl(dist);
+					distMat[j][i] = new IntVbl(dist);
+
+				}
+			}
+		});
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+
+				System.out.print(distMat[i][j].item);
+				if (j != n - 1)
+					System.out.print(" ");
+			}
+			if (i != n - 1)
+				System.out.println();
+		}
+
+	}
 }

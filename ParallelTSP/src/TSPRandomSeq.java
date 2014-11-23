5f5278188d5eca6a706564fac504d183b00138d6
hsadhvani <hsadhvani@gmail.com>
2014-11-23 03:27:08 -0500

    tsp random

---
+++ ParallelTSP/src/TSPRandomSeq.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+import edu.rit.pj2.Job;
+import edu.rit.pj2.Task;
+import edu.rit.util.Random;
+
+public class TSPRandomSeq extends Job {
+
+	public void main(String[] args) {
+		rule().task(TSPRandomSequential.class).runInJobProcess().args(args);
+	}
+
+	//
+	public static class TSPRandomSequential extends Task {
+		public void main(String args[]) {
+
+			Scanner scan = new Scanner(System.in);
+
+			int n = scan.nextInt();
+
+			scan.nextLine();
+
+			String[] city = new String[n];
+			int[][] distance = new int[n][n];
+
+			for (int i = 0; i < n; i++) {
+				city[i] = scan.nextLine();
+			}
+
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					distance[i][j] = scan.nextInt();
+				}
+			}
+
+			scan.close();
+			int T = Integer.parseInt(args[0]);
+			int minDist = Integer.MAX_VALUE;
+			int randomValues[] = new int[n];
+			int finalValues[] = new int[n];
+
+			for (int i = 0; i < n; i++) {
+				randomValues[i] = i;
+			}
+			for (int i = 0; i < T; i++) {
+				int sum = 0;
+
+				for (int j = 1; j < n; j++) {
+					sum += distance[0][randomValues[j]];
+				}
+				if (sum < minDist) {
+					minDist = sum;
+					finalValues = randomValues;
+				}
+				arrayShuffle(randomValues, Integer.parseInt(args[1]));
+			}
+
+			for (int i = 0; i < n; i++) {
+				if (i != n - 1)
+					System.out.print(city[finalValues[i]] + " --> ");
+				else
+					System.out.print(city[finalValues[i]]);
+			}
+			System.out.println("\nTotal distance :"+minDist);
+
+		}
+
+		public void arrayShuffle(int[] arr, int R) {
+			int indexRandom, temp;
+			Random random = new Random(R);
+			for (int i = arr.length - 1; i > 0; i--) {
+				indexRandom = random.nextInt(i + 1);
+				temp = arr[indexRandom];
+				arr[indexRandom] = arr[i];
+				arr[i] = temp;
+			}
+		}
+	}
+
+}
